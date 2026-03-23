ed669e6eb6a0e8781cd313ab81ea6b75b5eb3364
Sn0rrii <4687675+netscout2001@users.noreply.github.com>
2026-03-23 10:37:57 +0100

    run.sh aktualisieren

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Port aus HA Addon Config lesen
 PORT=$(curl -s -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" \
   http://supervisor/addons/self/info | \
   python3 -c "import sys,json; d=json.load(sys.stdin); print(d.get('data',{}).get('network',{}).get('8000/tcp', 8000))" 2>/dev/null || echo "8000")
